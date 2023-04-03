780e5a8e7cb4d238c7c38d911da2bb98ffa6d1be
Mykola-Ivanov <mykolaivanov0201@gmail.com>
2023-04-03 22:34:48 +0300

    update solve

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static String path = "src\\main\\resources\\L3_2.txt";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //List<TSPBranchAndBound> list = new ArrayList<>();
+        try {
+            //for (int j = 0; ; j++) {
+            GraphLoader loader = new GraphLoader(path, true);
+            TSPBranchAndBound bnb = new TSPBranchAndBound(loader.getAdjacencyMatrix());
+            bnb.solve(1);
+            System.out.print("Маршрут:\t");
+            bnb.getBestPath().stream().forEach(i -> System.out.print(i + "\t"));
+            System.out.println("\nціна маршруту:\t" + bnb.getMinimumCost());
+                //list.add(bnb);
+                //if (j + 1 == loader.getAdjacencyMatrix().length)
+                //    break;
+            //}
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
